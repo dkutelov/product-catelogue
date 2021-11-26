a183a7202236c6117dc5e6a5c8844c982ce58ae8
Dari <dari.k@abv.bg>
2021-11-26 15:16:54 +0200

    Header - add contact

--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -11,13 +11,17 @@ const Header = () => {
             <Link href='/'>
               <h1>Разпродажба Бебешки Стоки</h1>
             </Link>
+            <p className={styles.subHeading}>
+              За поръчка: 0889621010, contact@digitals.bg. Доставка с куриер:
+              4.95лв.
+            </p>
           </li>
-          <li className={styles.newProductLink}>
+          {/* <li className={styles.newProductLink}>
             <Link href='/new'>Нов</Link>
           </li>
           <li className={styles.newProductLink}>
             <Link href='/edit'>Редакция</Link>
-          </li>
+          </li> */}
         </ul>
       </nav>
     </header>
